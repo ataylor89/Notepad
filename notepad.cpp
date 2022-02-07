412066eb8b28e062f1a8eccc13c5466f4134a852
Andrew Taylor <rolyata47@gmail.com>
2022-02-06 21:54:54 -0600

    Adding code for Notepad

---
+++ notepad.cpp
@@ -0,0 +1,114 @@
+#include "notepad.h"
+#include "ui_notepad.h"
+
+Notepad::Notepad(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::Notepad)
+{
+    ui->setupUi(this);
+    this->setCentralWidget(ui->textEdit);
+
+    connect(ui->actionNew, &QAction::triggered, this, &Notepad::newDocument);
+    connect(ui->actionOpen, &QAction::triggered, this, &Notepad::open);
+    connect(ui->actionSave, &QAction::triggered, this, &Notepad::save);
+    connect(ui->actionSave_As, &QAction::triggered, this, &Notepad::saveAs);
+    connect(ui->actionExit, &QAction::triggered, this, &Notepad::exit);
+    connect(ui->actionCopy, &QAction::triggered, this, &Notepad::copy);
+    connect(ui->actionPaste, &QAction::triggered, this, &Notepad::paste);
+    connect(ui->actionUndo, &QAction::triggered, this, &Notepad::undo);
+    connect(ui->actionRedo, &QAction::triggered, this, &Notepad::redo);
+}
+
+Notepad::~Notepad()
+{
+    delete ui;
+}
+
+void Notepad::newDocument() {
+    currentFile.clear();
+    ui->textEdit->setText(QString());
+}
+
+void Notepad::open() {
+    QString fileName = QFileDialog::getOpenFileName(this, "Open the file");
+    QFile file(fileName);
+    currentFile = fileName;
+    if (!file.open(QIODevice::ReadOnly | QFile::Text)) {
+       QMessageBox::warning(this, "Warning", "Cannot open file: " + file.errorString());
+       return;
+    }
+    setWindowTitle(fileName);
+    QTextStream in(&file);
+    QString text = in.readAll();
+    ui->textEdit->setText(text);
+    file.close();
+}
+
+void Notepad::save()
+{
+    QString fileName;
+    // If we don't have a filename from before, get one.
+    if (currentFile.isEmpty()) {
+        fileName = QFileDialog::getSaveFileName(this, "Save");
+        currentFile = fileName;
+    } else {
+        fileName = currentFile;
+    }
+    QFile file(fileName);
+    if (!file.open(QIODevice::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void Notepad::saveAs()
+{
+    QString fileName = QFileDialog::getSaveFileName(this, "Save as");
+    QFile file(fileName);
+
+    if (!file.open(QFile::WriteOnly | QFile::Text)) {
+        QMessageBox::warning(this, "Warning", "Cannot save file: " + file.errorString());
+        return;
+    }
+    currentFile = fileName;
+    setWindowTitle(fileName);
+    QTextStream out(&file);
+    QString text = ui->textEdit->toPlainText();
+    out << text;
+    file.close();
+}
+
+void Notepad::exit()
+{
+    QCoreApplication::quit();
+}
+
+void Notepad::copy()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->copy();
+#endif
+}
+
+void Notepad::paste()
+{
+#if QT_CONFIG(clipboard)
+    ui->textEdit->paste();
+#endif
+}
+
+void Notepad::undo()
+{
+     ui->textEdit->undo();
+}
+
+void Notepad::redo()
+{
+    ui->textEdit->redo();
+}
+
---
+++ notepad.h
@@ -0,0 +1,35 @@
+#ifndef NOTEPAD_H
+#define NOTEPAD_H
+
+#include <QMainWindow>
+#include <QFileDialog>
+#include <QMessageBox>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Notepad; }
+QT_END_NAMESPACE
+
+class Notepad : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    Notepad(QWidget *parent = nullptr);
+    ~Notepad();
+
+private slots:
+    void newDocument();
+    void open();
+    void save();
+    void saveAs();
+    void exit();
+    void copy();
+    void paste();
+    void undo();
+    void redo();
+
+private:
+    Ui::Notepad *ui;
+    QString currentFile;
+};
+#endif // NOTEPAD_H
